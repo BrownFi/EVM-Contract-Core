8140d6671398eab8c47d8b0ff000ab4455a08462
Ekoios-Evan <an.nong@ekoios.vn>
2024-02-16 16:51:18 +0700

    feat: update swap formula

--- contracts/BrownfiV2Factory.sol
+++ contracts/BrownfiV2Factory.sol
@@ -1,9 +1,9 @@
 pragma solidity =0.5.16;
 
-import './interfaces/IUniswapV2Factory.sol';
-import './UniswapV2Pair.sol';
+import './interfaces/IBrownfiV2Factory.sol';
+import './BrownfiV2Pair.sol';
 
-contract UniswapV2Factory is IUniswapV2Factory {
+contract BrownfiV2Factory is IBrownfiV2Factory {
     address public feeTo;
     address public feeToSetter;
     address public kSetter;
@@ -23,16 +23,16 @@ contract UniswapV2Factory is IUniswapV2Factory {
     }
 
     function createPair(address tokenA, address tokenB) external returns (address pair) {
-        require(tokenA != tokenB, 'UniswapV2: IDENTICAL_ADDRESSES');
+        require(tokenA != tokenB, 'BrownfiV2: IDENTICAL_ADDRESSES');
         (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-        require(token0 != address(0), 'UniswapV2: ZERO_ADDRESS');
-        require(getPair[token0][token1] == address(0), 'UniswapV2: PAIR_EXISTS'); // single check is sufficient
-        bytes memory bytecode = type(UniswapV2Pair).creationCode;
+        require(token0 != address(0), 'BrownfiV2: ZERO_ADDRESS');
+        require(getPair[token0][token1] == address(0), 'BrownfiV2: PAIR_EXISTS'); // single check is sufficient
+        bytes memory bytecode = type(BrownfiV2Pair).creationCode;
         bytes32 salt = keccak256(abi.encodePacked(token0, token1));
         assembly {
             pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
         }
-        IUniswapV2Pair(pair).initialize(token0, token1);
+        IBrownfiV2Pair(pair).initialize(token0, token1);
         getPair[token0][token1] = pair;
         getPair[token1][token0] = pair; // populate mapping in the reverse direction
         allPairs.push(pair);
@@ -40,17 +40,17 @@ contract UniswapV2Factory is IUniswapV2Factory {
     }
 
     function setFeeTo(address _feeTo) external {
-        require(msg.sender == feeToSetter, 'UniswapV2: FORBIDDEN');
+        require(msg.sender == feeToSetter, 'BrownfiV2: FORBIDDEN');
         feeTo = _feeTo;
     }
 
     function setFeeToSetter(address _feeToSetter) external {
-        require(msg.sender == feeToSetter, 'UniswapV2: FORBIDDEN');
+        require(msg.sender == feeToSetter, 'BrownfiV2: FORBIDDEN');
         feeToSetter = _feeToSetter;
     }
 
     function setKSetter(address _kSetter) external {
-        require(msg.sender == kSetter, 'UniswapV2: FORBIDDEN');
+        require(msg.sender == kSetter, 'BrownfiV2: FORBIDDEN');
         kSetter = _kSetter;
     }
 }
--- contracts/BrownfiV2Pair.sol
+++ contracts/BrownfiV2Pair.sol
@@ -1,14 +1,14 @@
 pragma solidity =0.5.16;
 
-import './interfaces/IUniswapV2Pair.sol';
-import './UniswapV2ERC20.sol';
+import './interfaces/IBrownfiV2Pair.sol';
+import './BrownfiV2ERC20.sol';
 import './libraries/Math.sol';
 import './libraries/UQ112x112.sol';
 import './interfaces/IERC20.sol';
-import './interfaces/IUniswapV2Factory.sol';
-import './interfaces/IUniswapV2Callee.sol';
+import './interfaces/IBrownfiV2Factory.sol';
+import './interfaces/IBrownfiV2Callee.sol';
 
-contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
+contract BrownfiV2Pair is IBrownfiV2Pair, BrownfiV2ERC20 {
     using SafeMath  for uint;
     using UQ112x112 for uint224;
 
@@ -29,18 +29,18 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
 
     // new variables 
     uint public pPost; // post trade price
-    uint public k = 20000; // adjustable parameter
+    uint public k = 2000; // adjustable parameter
 
     uint private unlocked = 1;
     modifier lock() {
-        require(unlocked == 1, 'UniswapV2: LOCKED');
+        require(unlocked == 1, 'BrownfiV2: LOCKED');
         unlocked = 0;
         _;
         unlocked = 1;
     }
 
     function setK(uint _k) external {
-        require(msg.sender == IUniswapV2Factory(factory).kSetter(), 'UniswapV2: FORBIDDEN');
+        require(msg.sender == IBrownfiV2Factory(factory).kSetter(), 'BrownfiV2: FORBIDDEN');
         k = _k;
     }
 
@@ -52,7 +52,7 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
 
     function _safeTransfer(address token, address to, uint value) private {
         (bool success, bytes memory data) = token.call(abi.encodeWithSelector(SELECTOR, to, value));
-        require(success && (data.length == 0 || abi.decode(data, (bool))), 'UniswapV2: TRANSFER_FAILED');
+        require(success && (data.length == 0 || abi.decode(data, (bool))), 'BrownfiV2: TRANSFER_FAILED');
     }
 
     event Mint(address indexed sender, uint amount0, uint amount1);
@@ -73,14 +73,14 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
 
     // called once by the factory at time of deployment
     function initialize(address _token0, address _token1) external {
-        require(msg.sender == factory, 'UniswapV2: FORBIDDEN'); // sufficient check
+        require(msg.sender == factory, 'BrownfiV2: FORBIDDEN'); // sufficient check
         token0 = _token0;
         token1 = _token1;
     }
 
     // update reserves and, on the first call per block, price accumulators
     function _update(uint balance0, uint balance1, uint112 _reserve0, uint112 _reserve1) private {
-        require(balance0 <= uint112(-1) && balance1 <= uint112(-1), 'UniswapV2: OVERFLOW');
+        require(balance0 <= uint112(-1) && balance1 <= uint112(-1), 'BrownfiV2: OVERFLOW');
         uint32 blockTimestamp = uint32(block.timestamp % 2**32);
         uint32 timeElapsed = blockTimestamp - blockTimestampLast; // overflow is desired
         if (timeElapsed > 0 && _reserve0 != 0 && _reserve1 != 0) {
@@ -96,7 +96,7 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
 
     // if fee is on, mint liquidity equivalent to 1/6th of the growth in sqrt(k)
     function _mintFee(uint112 _reserve0, uint112 _reserve1) private returns (bool feeOn) {
-        address feeTo = IUniswapV2Factory(factory).feeTo();
+        address feeTo = IBrownfiV2Factory(factory).feeTo();
         feeOn = feeTo != address(0);
         uint _kLast = kLast; // gas savings
         if (feeOn) {
@@ -131,7 +131,7 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
         } else {
             liquidity = Math.min(amount0.mul(_totalSupply) / _reserve0, amount1.mul(_totalSupply) / _reserve1);
         }
-        require(liquidity > 0, 'UniswapV2: INSUFFICIENT_LIQUIDITY_MINTED');
+        require(liquidity > 0, 'BrownfiV2: INSUFFICIENT_LIQUIDITY_MINTED');
         _mint(to, liquidity);
 
         _update(balance0, balance1, _reserve0, _reserve1);
@@ -152,7 +152,7 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
         uint _totalSupply = totalSupply; // gas savings, must be defined here since totalSupply can update in _mintFee
         amount0 = liquidity.mul(balance0) / _totalSupply; // using balances ensures pro-rata distribution
         amount1 = liquidity.mul(balance1) / _totalSupply; // using balances ensures pro-rata distribution
-        require(amount0 > 0 && amount1 > 0, 'UniswapV2: INSUFFICIENT_LIQUIDITY_BURNED');
+        require(amount0 > 0 && amount1 > 0, 'BrownfiV2: INSUFFICIENT_LIQUIDITY_BURNED');
         _burn(address(this), liquidity);
         _safeTransfer(_token0, to, amount0);
         _safeTransfer(_token1, to, amount1);
@@ -166,37 +166,36 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
 
     // this low-level function should be called from a contract which performs important safety checks
     function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external lock {
-        require(amount0Out > 0 || amount1Out > 0, 'UniswapV2: INSUFFICIENT_OUTPUT_AMOUNT');
+        require(amount0Out > 0 || amount1Out > 0, 'BrownfiV2: INSUFFICIENT_OUTPUT_AMOUNT');
         (uint112 _reserve0, uint112 _reserve1,) = getReserves(); // gas savings
-        require(amount0Out < _reserve0 && amount1Out < _reserve1, 'UniswapV2: INSUFFICIENT_LIQUIDITY');
+        require(amount0Out < _reserve0 && amount1Out < _reserve1, 'BrownfiV2: INSUFFICIENT_LIQUIDITY');
 
         uint balance0;
         uint balance1;
         { // scope for _token{0,1}, avoids stack too deep errors
         address _token0 = token0;
         address _token1 = token1;
-        require(to != _token0 && to != _token1, 'UniswapV2: INVALID_TO');
+        require(to != _token0 && to != _token1, 'BrownfiV2: INVALID_TO');
         if (amount0Out > 0) _safeTransfer(_token0, to, amount0Out); // optimistically transfer tokens
         if (amount1Out > 0) _safeTransfer(_token1, to, amount1Out); // optimistically transfer tokens
-        if (data.length > 0) IUniswapV2Callee(to).uniswapV2Call(msg.sender, amount0Out, amount1Out, data);
+        if (data.length > 0) IBrownfiV2Callee(to).BrownfiV2Call(msg.sender, amount0Out, amount1Out, data);
         balance0 = IERC20(_token0).balanceOf(address(this));
         balance1 = IERC20(_token1).balanceOf(address(this));
         }
         uint amount0In = balance0 > _reserve0 - amount0Out ? balance0 - (_reserve0 - amount0Out) : 0;
         uint amount1In = balance1 > _reserve1 - amount1Out ? balance1 - (_reserve1 - amount1Out) : 0;
-        require(amount0In > 0 || amount1In > 0, 'UniswapV2: INSUFFICIENT_INPUT_AMOUNT');
+        require(amount0In > 0 || amount1In > 0, 'BrownfiV2: INSUFFICIENT_INPUT_AMOUNT');
         { // scope for reserve{0,1}Adjusted, avoids stack too deep errors
         uint balance0Adjusted = balance0.mul(10000).sub(amount0In.mul(25)); // update fee 0.5%
         uint balance1Adjusted = balance1.mul(10000).sub(amount1In.mul(25)); // update fee 0.5%
-        require(balance0Adjusted.mul(balance1Adjusted) >= uint(_reserve0).mul(_reserve1).mul(10000**2), 'UniswapV2: K');
+        require(balance0Adjusted.mul(balance1Adjusted) >= uint(_reserve0).mul(_reserve1).mul(10000**2), 'BrownfiV2: K');
         }
 
         // we should match balance to reserve, won't use (x-Dx, y-Dy) 
         _update(balance0, balance1, _reserve0, _reserve1);
         // update post trade price
         {
-        uint r = R(amount0Out, _reserve0, k);
-        pPost = postP(_reserve0, _reserve1, r);
+        pPost = postP(_reserve0, _reserve1, amount0Out, k);
         }
         emit Swap(msg.sender, amount0In, amount1In, amount0Out, amount1Out, to);
     }
@@ -214,20 +213,17 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
         _update(IERC20(token0).balanceOf(address(this)), IERC20(token1).balanceOf(address(this)), reserve0, reserve1);
     }
 
-    // price impact
-    function R(uint amountOut, uint reserveOut, uint _k) public pure returns (uint r){
-        require(amountOut < reserveOut, 'UniswapV2Library: INSUFFICIENT_OUTPUT_AMOUNT');
-        uint numerator = _k.mul(amountOut);
-        uint denominator = reserveOut.sub(amountOut).mul(10000);
-        r = numerator / denominator + 1;
-        return r.add(1);
+    // post trade price
+    function postP(uint reserveIn, uint reserveOut, uint amountOut, uint _k) internal pure returns (uint p){
+        uint numerator = reserveOut.mul(4000).mul(reserveOut.sub(amountOut));
+        // denominator = reserveIn*(4000*(reserveOut - amountOut) + 3*k +3*amountOut)
+        // = reserveIn*4000*reserveOut - reserveIn*4000*amountOut + 3*reserveIn*k + 3*reserveIn*amountOut
+        uint denominator = reserveIn.mul(4000).mul(reserveOut);
+        denominator = denominator.sub(reserveIn.mul(4000).mul(amountOut));
+        denominator = denominator.add(reserveIn.mul(3).mul(_k));
+        denominator = denominator.add(reserveIn.mul(3).mul(amountOut));
+        p = numerator / denominator;
+        return p;
     }
 
-    // post trade price
-    function postP(uint reserveIn, uint reserveOut, uint r) internal pure returns (uint p){
-        uint numerator = r.mul(3).add(4).mul(reserveOut);
-        uint denominator = reserveIn.mul(4);
-        p = numerator / denominator;
-        return p.add(1);
-    }
 }
