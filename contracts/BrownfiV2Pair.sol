38677450000e60e334d850eabd7c8aed7679b8b3
Ekoios-Evan <an.nong@ekoios.vn>
2024-02-16 20:56:50 +0700

    fix: update fee for amountOut

--- contracts/BrownfiV2Pair.sol
+++ contracts/BrownfiV2Pair.sol
@@ -176,8 +176,8 @@ contract BrownfiV2Pair is IBrownfiV2Pair, BrownfiV2ERC20 {
         address _token0 = token0;
         address _token1 = token1;
         require(to != _token0 && to != _token1, 'BrownfiV2: INVALID_TO');
-        if (amount0Out > 0) _safeTransfer(_token0, to, amount0Out); // optimistically transfer tokens
-        if (amount1Out > 0) _safeTransfer(_token1, to, amount1Out); // optimistically transfer tokens
+        if (amount0Out > 0) _safeTransfer(_token0, to, amount0Out.mul(9975)/10000); // optimistically transfer tokens
+        if (amount1Out > 0) _safeTransfer(_token1, to, amount1Out.mul(9975)/10000); // optimistically transfer tokens
         if (data.length > 0) IBrownfiV2Callee(to).BrownfiV2Call(msg.sender, amount0Out, amount1Out, data);
         balance0 = IERC20(_token0).balanceOf(address(this));
         balance1 = IERC20(_token1).balanceOf(address(this));
@@ -186,8 +186,8 @@ contract BrownfiV2Pair is IBrownfiV2Pair, BrownfiV2ERC20 {
         uint amount1In = balance1 > _reserve1 - amount1Out ? balance1 - (_reserve1 - amount1Out) : 0;
         require(amount0In > 0 || amount1In > 0, 'BrownfiV2: INSUFFICIENT_INPUT_AMOUNT');
         { // scope for reserve{0,1}Adjusted, avoids stack too deep errors
-        uint balance0Adjusted = balance0.mul(10000).sub(amount0In.mul(25)); // update fee 0.5%
-        uint balance1Adjusted = balance1.mul(10000).sub(amount1In.mul(25)); // update fee 0.5%
+        uint balance0Adjusted = balance0.mul(10000).sub(amount0In.mul(25)); // update fee 0.25% for one side
+        uint balance1Adjusted = balance1.mul(10000).sub(amount1In.mul(25)); // update fee 0.25% for one side
         require(balance0Adjusted.mul(balance1Adjusted) >= uint(_reserve0).mul(_reserve1).mul(10000**2), 'BrownfiV2: K');
         }
 
